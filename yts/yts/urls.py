aa283a0d1c838efce95c12c9f5132b7978f09767
Denis Karpelevich <dinka14@yandex.ru>
2014-03-15 18:49:22 +0400

    change urls.py 1849

--- yts/yts/urls.py
+++ yts/yts/urls.py
@@ -18,13 +18,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home,name='index'),
     url(r'^home/$', views.home),
-<<<<<<< HEAD
-    url(r'^blog/',include('ContributionBlog.urls')),
-    url(r'^cryptopage/',include('cryptopage.urls')),
-    url(r'^polls/',include('polls.urls')),                       
-=======
     url(r'^blog/',include('ContributionBlog.urls',namespace="blog")),
     url(r'^cryptopage/',include('cryptopage.urls',namespace="cryptopage")),
     url(r'^testme/', include('testme.urls', namespace="testme")),
->>>>>>> c73df655c998f34c22d250cfcc1cc27aaabc443f
+    url(r'^polls/',include('polls.urls', namespace="polls")),                   
 )  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
